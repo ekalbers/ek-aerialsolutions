246a02a06d916554ff82abeac4592090c571fe53
Ethan Albers <ekalbers22@gmail.com>
2023-07-26 11:28:20 -0400

    attempted fix for cookie

--- components/Contact.js
+++ components/Contact.js
@@ -5,24 +5,24 @@ export const csrfUrl = process.env.NEXT_PUBLIC_API_CSRFTOKEN
 
 const Contact = ({ toggleContact }) => {
   function getCookie(name) {
-    // let cookieValue = null;
-    // if (document.cookie && document.cookie !== '') {
-    //   const cookies = document.cookie.split(';');
-    //   for (let i = 0; i < cookies.length; i++) {
-    //     const cookie = cookies[i].trim();
-    //     // Check if the cookie name matches the CSRF cookie name (usually 'csrftoken')
-    //     if (cookie.substring(0, name.length + 1) === name + '=') {
-    //       cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-    //       break;
-    //     }
-    //   }
-    // }
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // Check if the cookie name matches the CSRF cookie name (usually 'csrftoken')
+        if (cookie.substring(0, name.length + 1) === name + '=') {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
 
-    // return cookieValue;
+    return cookieValue;
 
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
+    // const value = `; ${document.cookie}`;
+    // const parts = value.split(`; ${name}=`);
+    // if (parts.length === 2) return parts.pop().split(';').shift();
   }
 
   const handleSubmit = async (e) => {
