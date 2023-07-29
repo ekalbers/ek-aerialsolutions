3f93821f69c4f18fa6f017a060b689572705ab96
Ethan Albers <ekalbers22@gmail.com>
2023-07-29 18:38:57 -0400

    changed form data processing

--- components/Contact.js
+++ components/Contact.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 export const createUrl = process.env.NEXT_PUBLIC_API_CREATE;
 export const csrfUrl = process.env.NEXT_PUBLIC_API_CSRFTOKEN
 
 
 const Contact = ({ toggleContact }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    company: '',
+    email: '',
+    phone: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }))
+  }
+
   function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -27,27 +43,25 @@ const Contact = ({ toggleContact }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const company = document.getElementById('company').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const message = document.getElementById('message').value;
-    console.log(name)
-    console.log(company)
-    console.log(email)
-    console.log(phone)
-    console.log(message)
-    const req = new FormData();
-    req.append('name', name);
-    req.append('company', company);
-    req.append('email', email);
-    req.append('phone', phone)
-    req.append('message', message);
-    await console.log(JSON.stringify(req));
+    // const name = document.getElementById('name').value;
+    // const company = document.getElementById('company').value;
+    // const email = document.getElementById('email').value;
+    // const phone = document.getElementById('phone').value;
+    // const message = document.getElementById('message').value;
+    // console.log(name)
+    // console.log(company)
+    // console.log(email)
+    // console.log(phone)
+    // console.log(message)
+    // const req = new FormData();
+    // req.append('name', name);
+    // req.append('company', company);
+    // req.append('email', email);
+    // req.append('phone', phone)
+    // req.append('message', message);
+    // await console.log(JSON.stringify(req));
 
-
-    const url = createUrl;
-
+    console.log(JSON.stringify(formData));
 
     try {
       let csrf = await fetch(csrfUrl)
@@ -58,14 +72,14 @@ const Contact = ({ toggleContact }) => {
       // const headers = new Headers();
       // headers.append('X-CSRFToken', csrfToken);
       options.method = "POST";
-      options.body = req;
+      options.body = formData;
       options.credentials = 'include';
       // options.headers = {
       //   'Content-Type': 'application/json'
       // };
-      console.log(url);
+      console.log(createUrl);
       console.log(options);
-      const response = await fetch(url, options);
+      const response = await fetch(createUrl, options);
       console.log(response);
       console.log(await response.text());
     } catch (err) {
@@ -81,32 +95,32 @@ const Contact = ({ toggleContact }) => {
         <form className='w-2/6' onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-            <input type="text" id='name' name='name'
+            <input type="text" id='name' name='name' value={formData.name} onChange={handleChange}
                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                    required/>
           </div>
           <div className="mb-4">
             <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company</label>
-            <input type="text" id='company' name='company'
+            <input type="text" id='company' name='company' value={formData.company} onChange={handleChange}
                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                    required/>
           </div>
           <div className="mb-4">
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-            <input type="email" id='email' name='email'
+            <input type="email" id='email' name='email' value={formData.email} onChange={handleChange}
                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                    placeholder="email@email.com" required/>
           </div>
           <div className="mb-4">
             <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone (Optional)</label>
-            <input type='tel' id='phone' name='phone'
+            <input type='tel' id='phone' name='phone' value={formData.phone} onChange={handleChange}
                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                    placeholder="XXX-XXX-XXXX"/>
           </div>
           <div className="mb-4">
             <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Services Requested
               </label>
-            <textarea id='message' name='message' rows="4"
+            <textarea id='message' name='message' rows="4" value={formData.message} onChange={handleChange}
                       className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                       placeholder="Describe the project..."/>
           </div>
