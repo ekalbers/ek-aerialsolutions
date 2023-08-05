6fc6486f01703f168abec63d6751810bff962945
Ethan Albers <ekalbers22@gmail.com>
2023-08-05 17:26:51 -0400

    fixing meta data

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>E.K. Drones</title>
-        <meta name="description" content="Homepage for EK Drones" />
+        <title>EK Aerial Solutions - Drone Services for Aerial Mapping, 3D Modeling, and Photography</title>
+        <meta name="description" content="Unlock stunning perspectives with EK Aerial Solutions. Our drone services offer top-tier aerial photography and videography solutions. Capture the extraordinary from above. Visit us at www.ekaerialsolutions.com." />
         <meta name="viewport"content="width=device-width, initial-scale=1.0" />
         <link rel='icon' href='/ek-drones-website-favicon-white.png'/>
       </Head>
