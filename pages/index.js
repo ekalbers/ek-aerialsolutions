612e9050d03371b7e3d846cbce719c64ea72583b
Ethan Albers <ekalbers22@gmail.com>
2023-08-06 12:47:39 -0400

    adding analytics

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
         <title>EK Aerial Solutions - Drone Services Provider</title>
         <meta name="description" content="Unlock stunning perspectives with EK Aerial Solutions. Our drone services offer top-tier aerial photography, orthmosaic mapping, 3D Modeling, and More. Capture the extraordinary from above. Visit us at www.ekaerialsolutions.com." />
         <meta name="viewport"content="width=device-width, initial-scale=1.0" />
+        <meta name="ahrefs-site-verification" content="b1aaaaf4c137c95893ffb1fc663642599cbab96e6cf4acf5d9c67c21069a153b" />
         <link rel='icon' href='/ek-drones-website-favicon-white.png'/>
       </Head>
       <StateProvider>
