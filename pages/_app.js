accb5c19868e0d2fd10c82ce89313cbb6a519b96
Ethan Albers <ekalbers22@gmail.com>
2023-08-06 12:20:51 -0400

    adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '@/styles/globals.css'
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { Analytics} from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
       <Component {...pageProps} />
+      <Analytics />
     </UserProvider>
   )
 }
