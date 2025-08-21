48b8518920dbc4adeb7b4f7fc6d3e8410d241f3d
Ethan Albers <ekalbers22@gmail.com>
2025-08-21 01:37:36 -0400

    fixing typo

--- components/Thanks.js
+++ components/Thanks.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import {useUpdateStateContext} from "@/hooks/StateContext";
+import { useUpdateStateContext } from "@/hooks/StateContext";
 
 const Thanks = () => {
   const updateState = useUpdateStateContext()
@@ -24,7 +24,7 @@ const Thanks = () => {
         <p className="text-center mb-6 text-lg font-normal text-white lg:text-xl sm:px-16 xl:px-48">We will contact you in the next 24-48 hours to discuss your project!</p>
         <div className='text-center'>
           <button onClick={handleClick}
-             className="py-2.5 px-5 mr-2 mb-2 text-md font-medium text-gray-500 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-200 border-gray-600 hover:bg-gray-100">
+            className="py-2.5 px-5 mr-2 mb-2 text-md font-medium text-gray-500 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-200 border-gray-600 hover:bg-gray-100">
             Home
           </button>
         </div>
