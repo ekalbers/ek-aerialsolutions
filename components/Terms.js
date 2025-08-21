98d5d60ab81e022e6badca22c3b2b500f056f701
Ethan Albers <ekalbers22@gmail.com>
2025-08-21 01:10:35 -0400

    updates to TOS

--- components/Terms.js
+++ components/Terms.js
@@ -18,13 +18,21 @@ const Terms = () => {
 
     return (
         <>
-            <div className='items-center justify-center'>
+            <div className='items-center justify-center p-28'>
                 <h1
-                    className="text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Terms of Service:</h1>
-                <p className="text-center mb-6 text-lg font-normal text-white lg:text-xl sm:px-16 xl:px-48">Ownership and Usage<br />
+                    className="text-center mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Terms of Service:</h1>
+                <h2 className="text-left mb-2 text-lg font-bold text-white lg:text-xl sm:px-16 xl:px-48">Ownership and Usage:</h2>
+                <p className="text-left mb-6 text-lg font-normal text-white lg:text-xl sm:px-16 xl:px-48">
                     The client will have the right to use the final photos and videos for their intended purpose, whether it be for personal or commercial use. Provider retains the right to use the photos and videos for promotional purposes so long as Provider does not disclose the identity or any personal information of the Client without the written approval of the client.
                 </p>
-                <p className="text-center mb-6 text-lg font-normal text-white lg:text-xl sm:px-16 xl:px-48"></p>
+                <h2 className="text-left mb-2 text-lg font-bold text-white lg:text-xl sm:px-16 xl:px-48">Safe Practices:</h2>
+                <p className="text-left mb-6 text-lg font-normal text-white lg:text-xl sm:px-16 xl:px-48">
+                    Pilot follows FAA Part 107 Regulation to the best of their ability to include not flying over crowds of people or traffic.
+                </p>
+                <h2 className="text-left mb-2 text-lg font-bold text-white lg:text-xl sm:px-16 xl:px-48">Liability:</h2>
+                <p className="text-left mb-6 text-lg font-normal text-white lg:text-xl sm:px-16 xl:px-48">
+                    Provider represents and warrants that they have liability insurance coverage related to the provision of drone services. The details of this insurance coverage, including the coverage limits and policy number, can be provided upon client’s request.
+                </p>
             </div>
         </>
     );
